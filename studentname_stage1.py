e1d813bf4eb4a7f94ca127a07c6a4cd85504c6cc
Raja B <rajasudha407@gmail.com>
2026-02-26 21:57:30 +0530

    Create studentname_stage1.py

---
+++ studentname_stage1.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operator"
+
+print(f"Result = {result}")
