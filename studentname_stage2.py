18ef5d1246a512ef4be3b4bd2ff7c65b9ef588c4
Raja B <rajasudha407@gmail.com>
2026-02-26 22:05:48 +0530

    Create studentname_stage2.py

---
+++ studentname_stage2.py
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 5
+operator = "-"
+result = num1 - num2
+print(f"Result = {result}")
+if result > 0:
+    print("Positive")
+elif result < 0:
+    print("Negative")
+else:
+    print("Zero")
